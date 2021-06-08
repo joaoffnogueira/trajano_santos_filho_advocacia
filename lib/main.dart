d2d3923395638e70f48ddde0e0e14b24681898b9
joaoffnogueira <50890356+joaoffnogueira@users.noreply.github.com>
2021-06-08 16:51:48 -0300

    Added assets

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Trajano S. Filho Advocacia',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -20,9 +20,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.brown,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Trajano S. Filho Advocacia - Início'),
     );
   }
 }
@@ -100,6 +100,9 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
+            Image(image: AssetImage(
+              'assets/images/cover.jpg'
+            ))
           ],
         ),
       ),
