df59fc76c81ff00a39d4bf320fc14d52ff5ff591
joaoffnogueira <contact@joaonogueira.dev>
2022-03-19 15:33:33 -0300

    Add data about books and posts

---
+++ lib/data/ebooks.dart
@@ -0,0 +1,20 @@
+import '../models/ebook.dart';
+
+const EBOOKS_LIST = [
+  Ebook(
+    id: '1',
+    title:
+        'Compliance e LGPD – a importância da implementação de programas de governança em privacidade e proteção de dados pessoais',
+    image:
+        'https://tahech.com/wp-content/uploads/2021/12/Compliance-e-a-LGPD_-a-importancia-da-implementacao-de-programas-de-governanca-em-privacidade-e-protecao-de-dados-pessoais.pptx-1.jpg',
+    url:
+        'https://tahech.com/ebook/e-book-compliance-e-lgpd-a-importancia-da-implementacao-de-programas-de-governanca-em-privacidade-e-protecao-de-dados-pessoais/',
+  ),
+  Ebook(
+    id: '2',
+    title: 'A Regulamentação das Criptomoedas no Brasil – PL 2303/2015',
+    image: 'https://tahech.com/wp-content/uploads/2022/01/ebook_1-1.png',
+    url:
+        'https://tahech.com/ebook/a-regulamentacao-das-criptomoedas-no-brasil-pl-2303-2015/',
+  ),
+];
---
+++ lib/data/posts.dart
@@ -0,0 +1,11 @@
+import '../models/post.dart';
+
+const POSTS_LIST = [
+  Post(
+    id: '1',
+    title:
+        '3 motivos pelos quais todas as empresas atuantes no Brasil já deveriam estar adequadas à LGPD',
+    url:
+        'https://tahech.com/2022/03/11/3-motivos-pelos-quais-todas-as-empresas-atuantes-no-brasil-ja-deveriam-estar-adequadas-a-lgpd/',
+  ),
+];
