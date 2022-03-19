e6eddbc2dd369ee65bdafe17a69c10210e0006a3
joaoffnogueira <contact@joaonogueira.dev>
2022-03-19 15:33:15 -0300

    Create models for books and posts

---
+++ lib/models/ebook.dart
@@ -0,0 +1,13 @@
+class Ebook {
+  final String id;
+  final String title;
+  final String image;
+  final String url;
+
+  const Ebook({
+    required this.id,
+    required this.title,
+    required this.image,
+    required this.url,
+  });
+}
---
+++ lib/models/post.dart
@@ -0,0 +1,11 @@
+class Post {
+  final String id;
+  final String title;
+  final String url;
+
+  const Post({
+    required this.id,
+    required this.title,
+    required this.url,
+  });
+}
